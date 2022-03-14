6b08c065c84459ffc57dc95e22f9c8aa3b7a3b29
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-03-14 12:42:03 +0200

    Підняв state з Board d Game для можливості відображення історії ходів.

--- src/index.js
+++ src/index.js
@@ -14,53 +14,19 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            // перший хід "X"
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        // ігнорування натиску, якщо хтось переміг або поле повністю заповнене
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        // якщо xIsNext = true, ставить символ 'X', якщо xIsNext = false, то 'O'
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            // змінює значення xIsNext на протилежне
-            xIsNext: !this.state.xIsNext,
-        });
-    }
 
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        // перевіримо, чи гравехь виграв
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            // показує статус, який гравець має ходити 'X' чи 'O'
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -82,14 +48,61 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    // початковий state
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            // перший хід "X"
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        // ігнорування натиску, якщо хтось переміг або поле повністю заповнене
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        // якщо xIsNext = true, ставить символ 'X', якщо xIsNext = false, то 'O'
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            // змінює значення xIsNext на протилежне
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+
     render() {
+        // останній запис у історії гри для відображення статуса гри
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        // перевіримо, чи гравехь виграв
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
